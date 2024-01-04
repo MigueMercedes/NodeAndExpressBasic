9a6c559b0041f44e4acf990ed3a74bd3e8fd136e
Miguel Mercedes <lmyguel0324@gmail.com>
2024-01-04 11:26:05 -0400

    change: using body-parser package, not express.bodyParser
    undefined

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -10,8 +13,6 @@ app.use((req, res, next) => {
   response = `${req.method} ${req.path} - ${req.ip}`;
   next();
 });
-
-app.use(express.urlencoded({ extended: false }));
 
 const returnDate = (req, res, next) => {
   res.json({ date: new Date().toString() });
