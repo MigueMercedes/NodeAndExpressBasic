b54bda577712416656f1df398e46f07d7dee3f8d
Miguel Mercedes <lmyguel0324@gmail.com>
2024-01-04 09:10:20 -0400

    testing request methods

--- myApp.js
+++ myApp.js
@@ -1,23 +1,26 @@
 let express = require("express");
 let app = express();
-require('dotenv').config()
+require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"));
+
+let response;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  response = `${req.method} ${req.path} - ${req.ip}`;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
-    var response = {
-      "message": "Hello json"
-    }
-
-    if (process.env.MESSAGE_STYLE == "uppercase") {
-      response.message = response.message.toUpperCase();
-    }
-
-    res.json(response);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
+  }
+  res.json({ message: response });
 });
 
 module.exports = app;
